14a77974484e7c10ab39fef2ac1db5c393a388ce
Natalia <natalia.krysaa@gmail.com>
2023-03-06 12:09:06 +0100

    Initial commit

---
+++ src/main/java/com/app/demo/AppForEmployeeManagementApplication.java
@@ -0,0 +1,15 @@
+package com.app.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppForEmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppForEmployeeManagementApplication.class, args);
+	}
+
+}
